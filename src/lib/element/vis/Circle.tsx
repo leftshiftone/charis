e713cfc6bca051bb2ed165e4e72a1ff6dd867958
christian <christian.weber@leftshift.one>
2019-10-03 13:12:52 +0200

    Circle modified

--- src/lib/element/vis/Circle.tsx
+++ src/lib/element/vis/Circle.tsx
@@ -4,14 +4,22 @@ export default class Circle extends React.Component<CircleProps, {}> {
 
     public render(): React.ReactNode {
         return <svg className={"lto-circle " + (this.props.className || "")} viewBox="0 0 33.83098862 33.83098862" width="200" height="200">
-            <circle stroke="#efefef" strokeWidth="2" fill="none" cx="16.91549431" cy="16.91549431" r="15.91549431"/>
-            <circle stroke="#00acc1" strokeWidth="2" strokeDasharray={this.props.score * 100 + ",100"}
+            <circle stroke={this.props.innerColor || "#efefef"} strokeWidth="2" fill="none" cx="16.91549431" cy="16.91549431" r="15.91549431"/>
+            <circle stroke={this.props.outerColor || "#00acc1"} strokeWidth="2" strokeDasharray={this.props.score * 100 + ",100"}
                     strokeLinecap="round" fill="none" cx="16.91549431" cy="16.91549431" r="15.91549431"/>
             <g>
                 <text className="circle-chart__percent" x="16.91549431" y="15.5" alignmentBaseline="central"
                       textAnchor="middle"
                       fontSize="8">{parseFloat((this.props.score * 100).toString()).toFixed(0)}%
                 </text>
+                {
+                    this.props.text ?
+                        <text className="circle-chart__percent" x="16.91549431" y="20.5" alignmentBaseline="central"
+                              textAnchor="middle"
+                              fontSize="4">text
+                        </text> :
+                        <React.Fragment />
+                }
             </g>
         </svg>;
     }
@@ -21,4 +29,7 @@ interface CircleProps {
     score: number;
     heading?: string;
     className?:string;
+    innerColor?: string;
+    outerColor?: string;
+    text?: string;
 }
